e5d70f5afabf652a247f5b7c4e82722d8aba0270
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-31 16:53:41 +0200

    fix: update Next.js config and pin cheerio version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle the undici package
+    config.module.rules.push({
+      test: /node_modules\/undici\/.*\.js$/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
+  transpilePackages: ['undici']
 };
 
 module.exports = nextConfig; 
