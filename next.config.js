93a943a62ef04b1ea407a0b4353615620a188411
Michal Drajna <michal.drajna@bloomreach.com>
2025-03-31 17:06:24 +0200

    fix: remove custom CSS loaders and simplify webpack config

--- next.config.js
+++ next.config.js
@@ -7,17 +7,6 @@ const nextConfig = {
       type: 'javascript/auto',
     });
 
-    // Remove the existing CSS rule if it exists
-    config.module.rules = config.module.rules.filter(rule => 
-      !(rule.test && rule.test.toString() === /\.css$/.toString())
-    );
-
-    // Add the new CSS rule
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-
     return config;
   },
   transpilePackages: ['undici']
