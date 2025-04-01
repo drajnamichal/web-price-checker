81cd0356d1ee00a3439b3f992d187fedef874672
Michal Drajna <michal.drajna@bloomreach.com>
2025-04-01 08:23:37 +0200

    feat: add euro icon as favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Sledovač cien',
   description: 'Sledujte ceny produktov na internete',
+  icons: {
+    icon: '/euro_icon.ico'
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sk">
+      <head>
+        <link rel="icon" href="/euro_icon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
